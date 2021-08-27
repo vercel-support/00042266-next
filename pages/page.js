81b4a768ada6ec1d920a12d6e6e2b19d77638ead
paulogdm <paulogdemitri@gmail.com>
2021-08-27 11:28:33 -0300

    Update page.js

--- pages/page.js
+++ pages/page.js
@@ -12,18 +12,9 @@ return (
   
   // This gets called on every request
   export async function getServerSideProps() {
-    // Fetch data from external API
-    const res = await fetch(`https://www.boredapi.com/api/activity`)
-    const data = await res.json()
-
-    if (!data) {
-        return {
-          notFound: true,
-        }
-      }
-  
-    // Pass data to the page via props
-    return { props: { data } }
+    return {
+      notFound: true,
+    }
   }
   
   export default Page
