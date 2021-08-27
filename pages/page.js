082a00bdbd7792f603bd3021fdfb3586b540e2df
Riz <rizwana.akmal@hotmail.com>
2021-08-27 15:15:51 +0100

    fix

---
+++ pages/page.js
@@ -0,0 +1,23 @@
+function Page({ data }) {
+    // Render data...
+return (    
+    <>
+    <h1>{data.key}</h1>
+    <h1>{data.activity}</h1>
+    {console.log('data', data)}
+</>
+
+)
+  }
+  
+  // This gets called on every request
+  export async function getServerSideProps() {
+    // Fetch data from external API
+    const res = await fetch(`https://www.boredapi.com/api/activity`)
+    const data = await res.json()
+  
+    // Pass data to the page via props
+    return { props: { data } }
+  }
+  
+  export default Page
